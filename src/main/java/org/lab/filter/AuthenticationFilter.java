b2191a12223d76ff77a37a903da8256112a736a0
Your Name <you@example.com>
2025-05-13 12:30:23 +0300

    Final Project

---
+++ src/main/java/org/lab/filter/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package org.lab.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        String loginURI = httpRequest.getContextPath() + "/login";
+
+        boolean isLoggedIn = (session != null && session.getAttribute("userId") != null);
+        boolean isLoginRequest = httpRequest.getRequestURI().equals(loginURI);
+
+        if (isLoggedIn || isLoginRequest) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(loginURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
